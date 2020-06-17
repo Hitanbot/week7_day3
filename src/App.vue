15a5564e9c2647c55738516e622c7d92ff60f381
Hitanbot <matthewjamesworkmail@gmail.com>
2020-06-17 16:40:08 +0100

    mvp done, gonna try and upload mmore regularly

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,41 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+
+  <h1>Countries</h1>
+    <div class="main-container">
+      <countries-list :countries='countries'></countries-list>
+      <country-detail :country='selectedCountry'></country-detail>
+    </div>
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import CountriesList from './components/CountriesList.vue';
+import CountryDetail from './components/CountryDetail.vue';
+import { eventBus } from './main.js';
 
 export default {
   name: 'App',
+  data(){
+  return {
+    countries: [],
+    selectedCountry: null
+    };
+  },
+  mounted(){
+    fetch('https://restcountries.eu/rest/v2/all')
+    .then(res => res.json())
+    .then(countries => this.countries = countries)
+
+    eventBus.$on('country-selected', (country) => {
+    this.selectedCountry = country;
+    console.log(country)
+  })
+  },
   components: {
-    HelloWorld
+    'countries-list': CountriesList,
+    'country-detail': CountryDetail
   }
 }
 </script>
---
+++ src/components/CountryDetail.vue
@@ -0,0 +1,24 @@
+<template lang="html">
+
+  <div>
+  <div v-if='country'>
+    <h3>{{country.name}}</h3>
+    <p>Capital: {{country.capital}}</p>
+    <p>Region: {{country.region}}</p>
+    <p>Population: {{country.population}}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'country-detail',
+  props: ['country']
+}
+
+</script>
+
+<style lang="css" scoped>
+</style>
